6e61296af5d818b4a7594f97d0f225925caf74b9
VaneCB <vcarrerabcn@gmail.com>
2023-03-03 18:14:27 +0100

    cambios en el estilo

--- fin.php
+++ fin.php
@@ -9,7 +9,7 @@ $jugadas = $_SESSION['jugadas'];
 $win = $_GET['win'] ??"";
 $intento = sizeof ($_SESSION['jugadas']);
 if($win){
-    $msj ="<h1>FELICIDADES HAS ACERTADO EN $intento JUGADAS </h1>";
+    $msj = "<h1>FELICIDADES HAS ACERTADO EN $intento JUGADAS </h1>";
 }else{
     $msj = "<h1>¡HAS AGOTADO TODOS TUS INTENTOS!</h1>";
 
@@ -28,12 +28,28 @@ session_destroy();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Fin del juego</title>
     <link rel="stylesheet" href="./css/estilo.css" type="text/css">
     <link rel="stylesheet" href=".\node_modules\bootstrap\dist\css\bootstrap.min.css" type="text/css">
 
 </head>
 <body>
-    <?=$informacion?>
+
+<section class="historico border border-secondary">
+        <div class="container"> 
+        <div class="row">
+          <div class="col-md-4 text-start">
+            <h3>¡Fin del juego!</h3>
+            <p>¿Quieres volver a jugar?</p>
+            <div class="text-justify">
+             <a href="index.html"><button class="btn btn-primary text-center">Volver a jugar</button></a>
+            </div>
+          </div>
+          <div class="col-md-8 text-left">
+           <?=$informacion?>
+          </div>
+          </div>
+        </div>
+        </section>
 </body>
 </html>
